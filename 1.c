84d23fee41f5973e4918350baeaa6255c69e537b
Kevinjj65 <136084257+Kevinjj65@users.noreply.github.com>
2023-10-13 13:30:56 +0530

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,302 +1,234 @@
 #include <stdio.h>
-void polyadd(int A[], int B[], int C[], int m, int n)
-{
+
+// Function to convert a normal matrix into triplet form
+void NormalToSparse(int normal[][50], int sparse[][3], int rows, int cols) {
+    int nonZero = 0;
+    int counter = 1;
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            if (normal[i][j] != 0) {
+                nonZero++;
+                sparse[counter][0] = i;
+                sparse[counter][1] = j;
+                sparse[counter][2] = normal[i][j];
+                counter++;
+            }
+        }
+    }
+
+    sparse[0][0] = rows;
+    sparse[0][1] = cols;
+    sparse[0][2] = nonZero;
+}
+
+void printMatrixC(int mat[][3]) {
+    int rows = mat[0][0];
+    int cols = mat[0][1];
+    int nonZero = mat[0][2];
+
+    printf("Matrix C:\n");
+    printf("%d %d %d", rows, cols, nonZero);
+    printf("\n");
+    for (int i = 1; i <= nonZero; i++) {
+        printf("%d %d %d\n", mat[i][0], mat[i][1], mat[i][2]);
+    }
+}
+
+void printTransposeMatrix(int mat[][3]) {
+    int rows = mat[0][0];
+    int cols = mat[0][1];
+    int nonZero = mat[0][2];
+
+    printf("%d %d %d", rows, cols, nonZero);
+    printf("\n");
+    for (int i = 1; i <= nonZero; i++) {
+        printf("%d %d %d\n", mat[i][0], mat[i][1], mat[i][2]);
+    }
+}
+
+void AddSparseMatrix(int A[][3], int B[][3], int C[][3]) {
     int p = 1, q = 1, r = 1;
-    while (p <= 2 * m && q <= 2 * n)
-    {
-        if (A[p] == B[q])
-        {
-            C[r + 1] = A[p + 1] + B[q + 1];
-            if (C[r + 1] != 0)
-            {
-                C[r] = A[p];
-                r = r + 2;
-            }
-            p = p + 2;
-            q = q + 2;
-        }
-        else if (A[p] < B[q])
-        {
-            C[r + 1] = B[q + 1];
-            C[r] = B[q];
-            q = q + 2;
-            r = r + 2;
-        }
-        else
-        {
-            C[r + 1] = A[p + 1];
-            C[r] = A[p];
-            p = p + 2;
-            r = r + 2;
-        }
-    }
-    while (q <= 2 * n)
-    {
-        C[r] = B[q];
-        C[r + 1] = B[q + 1];
-        q = q + 2;
-        r = r + 2;
-    }
-    C[0] = r / 2;
-}
-int main()
-{
-    int A[50], B[50], C[50], l = 0;
-    int m, n, c, k;
-    printf("choice 1: to take values of polynomial\n choice 2: to sum and display\n");
-    while (l == 0)
-    {
-        printf("eneter the choice:");
-        scanf("%d", &k);
-        if (k == 1)
-        {
-            printf("enter the no. of terms in polynomail 1:");
-            scanf("%d", &m);
-            printf("enter the values of polynomial 1 in array form:");
-            for (int i = 1; i <= 2 * m; i += 2)
-            {
-                scanf("%d%d", &A[i], &A[i + 1]);
-            }
-            printf("enetr the no. of terms polynomial 2;");
-            scanf("%d", &n);
-            printf("eneter th values of polynomial 2 in array form:");
-            for (int i = 1; i <= 2 * n; i += 2)
-            {
-                scanf("%d%d", &B[i], &B[i + 1]);
-            }
-        }
-        else if (k == 2)
-        {
-            polyadd(A, B, C, m, n);
-            {
-                for (int i = 1; i <= C[0] * 2; i += 2)
-                {
-                    printf("%dx^%d", C[i + 1], C[i]);
-                    if (i < C[0] * 2 - 1)
-                    {
-                        printf("+");
-                    }
-                }
-                printf("\n");
-                return 0;
-            }
-        }
-        else
-        {
-            l += 1;
-        }
-    }
-}
 
-#include <stdio.h>
-void main()
-{
-    int ch, mat[50][50], row, a[50][3], b[50][3], c[3][3], p, q, r, m, n, t, i, j, col;
-    printf("choices:-\n");
-    printf("1. Sparse matrix addition\n");
-    printf("2. Sparse matrix transpose\n");
-    printf("Enter choice:\n");
-    scanf("%d", &ch);
-    if (ch == 1)
-    {
-        printf("Enter no. of rows of matrix 1:\n");
-        scanf("%d", &row);
-        printf("Enter no. of columns of matrix 1:\n");
-        scanf("%d", &col);
-        a[0][0] = row;
-        a[0][1] = col;
-        r = 1, p = 0;
-        for (i = 0; i < row; i++)
-        {
-            for (j = 0; j < col; j++)
-            {
-                printf("Enter value in row %d column %d:\n", i, j);
-                scanf("%d", &mat[i][j]);
-                if (mat[i][j] != 0)
-                {
-                    p++;
-                    a[r][0] = i;
-                    a[r][1] = j;
-                    a[r][2] = mat[i][j];
-                    r++;
-                }
-            }
-        }
-        a[0][2] = p;
-        printf("\nMatrix 1:\n\n");
-        for (i = 0; i <= a[0][2]; i++)
-        {
-            for (j = 0; j < 3; j++)
-                printf("%d\t", a[i][j]);
-            printf("\n");
-        }
-        printf("\nEnter no. of rows of matrix 2:\n");
-        scanf("%d", &row);
-        printf("Enter no. of columns of matrix 2:\n");
-        scanf("%d", &col);
-        b[0][0] = row;
-        b[0][1] = col;
-        r = 1, p = 0;
-        for (i = 0; i < row; i++)
-        {
-            for (j = 0; j < col; j++)
-            {
-                printf("Enter value in row %d column %d:\n", i, j);
-                scanf("%d", &mat[i][j]);
-                if (mat[i][j] != 0)
-                {
-                    p++;
-                    b[r][0] = i;
-                    b[r][1] = j;
-                    b[r][2] = mat[i][j];
-                    r++;
-                }
-            }
-        }
-        b[0][2] = p;
-        printf("\nMatrix 2:\n\n");
-
-        for (i = 0; i <= b[0][2]; i++)
-        {
-            for (j = 0; j < 3; j++)
-                printf("%d\t", b[i][j]);
-            printf("\n");
-        }
-        p = q = r = 1;
-        c[0][0] = a[0][0];
-        c[0][1] = b[0][1];
-        while (p <= a[0][2] && q <= b[0][2])
-        {
-            if (a[p][0] < b[q][0])
-            {
-                c[r][0] = a[p][0];
-                c[r][1] = a[p][1];
-                c[r][2] = a[p][2];
-                p++;
-                r++;
-            }
-            else if (a[p][0] > b[q][0])
-            {
-                c[r][0] = b[q][0];
-                c[r][1] = b[q][1];
-                c[r][2] = b[q][2];
-                q++;
-                r++;
-            }
-            else if (a[p][1] < b[q][1])
-            {
-                c[r][0] = a[p][0];
-                c[r][1] = a[p][1];
-                c[r][2] = a[p][2];
-                p++;
-                r++;
-            }
-            else if (a[p][1] > b[q][1])
-            {
-                c[r][0] = b[q][0];
-                c[r][1] = b[q][1];
-                c[r][2] = b[q][2];
-                q++;
-                r++;
-            }
-            else
-            {
-                c[r][0] = a[p][0];
-                c[r][1] = a[p][1];
-                c[r][2] = a[p][2] + b[q][2];
-                p++;
-                q++;
-                r++;
-            }
-        }
-        while (p <= a[0][2])
-        {
-            c[r][0] = a[p][0];
-            c[r][1] = a[p][1];
-            c[r][2] = a[p][2];
+    while (p <= A[0][2] && q <= B[0][2]) {
+        if (A[p][0] < B[q][0]) {
+            C[r][0] = A[p][0];
+            C[r][1] = A[p][1];
+            C[r][2] = A[p][2];
             p++;
             r++;
-        }
-        while (q <= b[0][2])
-        {
-            c[r][0] = b[q][0];
-            c[r][1] = b[q][1];
-            c[r][2] = b[q][2];
+        } else if (A[p][0] > B[q][0]) {
+            C[r][0] = B[q][0];
+            C[r][1] = B[q][1];
+            C[r][2] = B[q][2];
             q++;
             r++;
-        }
-        c[0][2] = r - 1;
-        printf("\nSum:\n\n");
-        for (i = 0; i < r; i++)
-        {
-            for (j = 0; j < 3; j++)
-                printf("%d\t", c[i][j]);
-            printf("\n");
-        }
-    }
-    else if (ch == 2)
-    {
-        printf("Enter no. of rows of matrix:\n");
-        scanf("%d", &row);
-        printf("Enter no. of columns of matrix:\n");
-        scanf("%d", &col);
-        a[0][0] = row;
-        a[0][1] = col;
-        r = 1;
-        for (i = 0; i < row; i++)
-        {
-            for (j = 0; j < col; j++)
-            {
-                printf("Enter value in row %d column %d:\n", i, j);
-                scanf("%d", &mat[i][j]);
-                if (mat[i][j] != 0)
-                {
-                    p++;
-                    a[r][0] = i;
-                    a[r][1] = j;
-                    a[r][2] = mat[i][j];
-                    r++;
-                }
+        } else {
+            if (A[p][1] < B[q][1]) {
+                C[r][0] = A[p][0];
+                C[r][1] = A[p][1];
+                C[r][2] = A[p][2];
+                p++;
+                r++;
+            } else if (A[p][1] > B[q][1]) {
+                C[r][0] = B[q][0];
+                C[r][1] = B[q][1];
+                C[r][2] = B[q][2];
+                q++;
+                r++;
+            } else {
+                C[r][0] = A[p][0];
+                C[r][1] = A[p][1];
+                C[r][2] = A[p][2] + B[q][2];
+                p++;
+                q++;
+                r++;
             }
         }
-        a[0][2] = p;
-        printf("\nOriginal sparse matrix:\n\n");
-        for (i = 0; i <= a[0][2]; i++)
-        {
-            for (j = 0; j < 3; j++)
-                printf("%d\t", a[i][j]);
-            printf("\n");
-        }
-        m = a[0][0];
-        n = a[0][1];
-        t = a[0][2];
-        b[0][0] = n;
-        b[0][1] = m;
-        b[0][2] = t;
-        q = 1;
-        if (t <= 0)
-            return;
-        for (col = 0; col <= n - 1; col++)
-        {
-            for (p = 1; p <= t; p++)
-            {
-                if (a[p][1] == col)
-                {
-                    b[q][0] = a[p][1];
-                    b[q][1] = a[p][0];
-                    b[q][2] = a[p][2];
-                    q++;
-                }
-            }
-        }
-        printf("\nTransposed sparse matrix:\n\n");
-        for (i = 0; i <= b[0][2]; i++)
-        {
-            for (int j = 0; j < 3; j++)
-                printf("%d\t", b[i][j]);
-            printf("\n");
-        }
     }
-    else
-        printf("\nWrong choice\n");
+
+    while (p <= A[0][2]) {
+        C[r][0] = A[p][0];
+        C[r][1] = A[p][1];
+        C[r][2] = A[p][2];
+        p++;
+        r++;
+    }
+
+    while (q <= B[0][2]) {
+        C[r][0] = B[q][0];
+        C[r][1] = B[q][1];
+        C[r][2] = B[q][2];
+        q++;
+        r++;
+    }
+
+    C[0][0] = A[0][0];
+    C[0][1] = A[0][1];
+    C[0][2] = r - 1;
+}
+
+void TransposeSparseMatrix(int mat[][3], int transposed[][3]) {
+    int rows = mat[0][0];
+    int cols = mat[0][1];
+    int nonZero = mat[0][2];
+
+    // Initialize an array to store the number of non-zero elements in each column
+    int columnCounts[cols];
+    for (int i = 0; i < cols; i++) {
+        columnCounts[i] = 0;
+    }
+
+    // Count the number of non-zero elements in each column
+    for (int i = 1; i <= nonZero; i++) {
+        int col = mat[i][1];
+        columnCounts[col]++;
+    }
+
+    // Calculate the starting position for each column in the transposed matrix
+    int startingPosition[cols];
+    startingPosition[0] = 1;
+    for (int i = 1; i < cols; i++) {
+        startingPosition[i] = startingPosition[i - 1] + columnCounts[i - 1];
+    }
+
+    // Transpose the matrix
+    transposed[0][0] = cols;
+    transposed[0][1] = rows;
+    transposed[0][2] = nonZero;
+
+    for (int i = 1; i <= nonZero; i++) {
+        int col = mat[i][1];
+        int pos = startingPosition[col];
+        transposed[pos][0] = col;
+        transposed[pos][1] = mat[i][0];
+        transposed[pos][2] = mat[i][2];
+        startingPosition[col]++;
+    }
+}
+
+int main() {
+    int ch;
+    int A[50][3], B[50][3], C[100][3], transposed[100][3];
+    int rowsA, colsA, rowsB, colsB;
+
+    do {
+        printf("\nMenu:\n");
+        printf("1. Sparse Matrix Addition\n");
+        printf("2. Sparse Matrix Transpose\n");
+        printf("3. Quit\n");
+        printf("Enter choice: ");
+        scanf("%d", &ch);
+
+        switch (ch) {
+            case 1:
+                printf("Enter the number of rows and columns for Matrix A: ");
+                scanf("%d %d", &rowsA, &colsA);
+                int normalMatrixA[50][50];
+
+                printf("Enter the elements of Matrix A (%d x %d):\n", rowsA, colsA);
+                for (int i = 0; i < rowsA; i++) {
+                    for (int j = 0; j < colsA; j++) {
+                        scanf("%d", &normalMatrixA[i][j]);
+                    }
+                }
+
+                NormalToSparse(normalMatrixA, A, rowsA, colsA);
+
+                printf("Enter the number of rows and columns for Matrix B: ");
+                scanf("%d %d", &rowsB, &colsB);
+                int normalMatrixB[50][50];
+
+                printf("Enter the elements of Matrix B (%d x %d):\n", rowsB, colsB);
+                for (int i = 0; i < rowsB; i++) {
+                    for (int j = 0; j < colsB; j++) {
+                        scanf("%d", &normalMatrixB[i][j]);
+                    }
+                }
+
+                NormalToSparse(normalMatrixB, B, rowsB, colsB);
+
+                printf("MATRIX A in Sparse Form:\n");
+                printTransposeMatrix(A);
+
+                printf("MATRIX B in Sparse Form:\n");
+                printTransposeMatrix(B);
+
+                AddSparseMatrix(A, B, C);
+                printf("Resultant Sparse Matrix C (A + B):\n");
+                printMatrixC(C);
+                break;
+
+            case 2:
+                printf("Enter the number of rows and columns for the matrix: ");
+                scanf("%d %d", &rowsA, &colsA);
+                int normalMatrix[50][50];
+
+                printf("Enter the elements of the matrix (%d x %d):\n", rowsA, colsA);
+                for (int i = 0; i < rowsA; i++) {
+                    for (int j = 0; j < colsA; j++) {
+                        scanf("%d", &normalMatrix[i][j]);
+                    }
+                }
+
+                NormalToSparse(normalMatrix, A, rowsA, colsA);
+
+                printf("Original Matrix in Sparse Form:\n");
+                printMatrixC(A);
+
+                TransposeSparseMatrix(A, transposed);
+                printf("Transposed Sparse Matrix:\n");
+                printTransposeMatrix(transposed);
+                break;
+
+            case 3:
+                printf("Quitting the program.\n");
+                break;
+
+            default:
+                printf("Invalid choice, please try again.\n");
+        }
+    } while (ch != 3);
+
+    return 0;
 }
